d47605655dde623decf8caef98c364d695402f2a
thajime0125 <thajime0125@gmail.com>
2023-05-14 22:22:31 +0900

    実行時にファイルを選択できるように変更

---
+++ main.py
@@ -0,0 +1,21 @@
+import sys
+
+import cv2
+import mediapipe as mp
+
+from pose_estimation.sample import mp_sample
+
+VIDEO_PATH_LIST = [
+    'data/20230514192027.mp4',
+    'data/IMG_9417.MOV',
+]
+
+
+def main(video_path):
+    mp_sample(video_path)
+
+
+if __name__ == '__main__':
+    _movie_index = int(sys.argv[1])
+    main(VIDEO_PATH_LIST[_movie_index])
+
--- pose_estimation/sample.py
+++ pose_estimation/sample.py
@@ -1,36 +1,39 @@
 import cv2
 import mediapipe as mp
 
-model_path = 'models/pose_landmarker_lite.task'
-video_path = 'data/20230514192027.mp4'
+# model_path = 'models/pose_landmarker_lite.task'
+VIDEO_PATH = 'data/20230514192027.mp4'
 
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_pose = mp.solutions.pose
+def mp_sample(video_path):
 
-# Webカメラから入力
-cap = cv2.VideoCapture(video_path)
-with mp_pose.Pose(
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as pose:
-  while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-      print("Ignoring empty camera frame.")
-      continue
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = pose.process(image)
- 
-    # 検出されたポーズの骨格をカメラ画像に重ねて描画
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    mp_drawing.draw_landmarks(
-        image,
-        results.pose_landmarks,
-        mp_pose.POSE_CONNECTIONS,
-        landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-    cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
-    if cv2.waitKey(5) & 0xFF == 27:
-      break
-cap.release()
+    mp_drawing = mp.solutions.drawing_utils
+    mp_drawing_styles = mp.solutions.drawing_styles
+    mp_pose = mp.solutions.pose
+
+    cap = cv2.VideoCapture(video_path)
+    with mp_pose.Pose(
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as pose:
+        while cap.isOpened():
+            success, image = cap.read()
+            if not success:
+                print("Ignoring empty camera frame.")
+                continue
+            image.flags.writeable = False
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            results = pose.process(image)
+
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            mp_drawing.draw_landmarks(
+                image,
+                results.pose_landmarks,
+                mp_pose.POSE_CONNECTIONS,
+                landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+            cv2.imshow('MediaPipe Pose', image)
+            if cv2.waitKey(5) & 0xFF == 27:
+                break
+        cap.release()
+
+if __name__ == '__main__':
+    mp_sample(VIDEO_PATH)
